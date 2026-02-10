126205520ea773a9aa0d671ecf90449f7d1d12c6
qhosting <admin@qhosting.net>
2026-02-10 13:51:22 -0600

    fix(build): update stripe apiVersion to 2026-01-28.clover

--- app/lib/stripe.ts
+++ app/lib/stripe.ts
@@ -2,6 +2,6 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
-    apiVersion: '2023-10-16', // Asegúrate de usar la versión correcta en dashboard
+    apiVersion: '2026-01-28.clover' as any,
     typescript: true,
 });
